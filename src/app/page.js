6594382f45b86ac53905c5f03058f7667689ddf3
khan-rustam <khanrustam2809@gmail.com>
2024-03-06 16:16:03 +0530

    added loading effect using next js

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,12 @@ const API_URL = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending';
+
   const res = await fetch(
     `https://api.themoviedb.org/3/${
       genre === 'fetchTopRated' ? 'movie/top_rated' : '/trending/all/week'
-    }?api_key=${API_URL}&language=hi-IN&page=1`
+    }?api_key=${API_URL}&language=hi-IN&page=1`,
+    { next: { revalidate: 10000 } }
   );
 
   const data = await res.json();
@@ -18,7 +20,7 @@ export default async function Home({ searchParams }) {
 
   return (
     <div>
-      <Results results={results}/>
+      <Results results={results} />
     </div>
   );
 }
